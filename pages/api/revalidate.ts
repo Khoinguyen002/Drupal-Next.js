c6b55095d925006b1d73867f4673f7dc71419367
khoinguyen <khoi.nguyen@kyanon.digital>
2024-07-14 02:58:00 +0700

    [feat] setup source code

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,29 +1,30 @@
-import { NextApiRequest, NextApiResponse } from "next"
+import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
-  request: NextApiRequest,
-  response: NextApiResponse
+	request: NextApiRequest,
+	response: NextApiResponse
 ) {
-  let slug = request.query.slug as string
-  const secret = request.query.secret as string
+	let slug = request.query.slug as string;
+	const secret = request.query.secret as string;
 
-  // Validate secret.
-  if (secret !== process.env.DRUPAL_REVALIDATE_SECRET) {
-    return response.status(401).json({ message: "Invalid secret." })
-  }
+	// Validate secret.
+	if (secret !== process.env.DRUPAL_REVALIDATE_SECRET) {
+		return response.status(401).json({ message: "Invalid secret." });
+	}
 
-  // Validate slug.
-  if (!slug) {
-    return response.status(400).json({ message: "Invalid slug." })
-  }
+	// Validate slug.
+	if (!slug) {
+		return response.status(400).json({ message: "Invalid slug." });
+	}
 
-  try {
-    await response.revalidate(slug)
+	try {
+		await response.revalidate(slug);
+		console.log("Revalidate Successfully");
 
-    return response.json({})
-  } catch (error) {
-    return response.status(404).json({
-      message: error.message,
-    })
-  }
+		return response.json({});
+	} catch (error) {
+		return response.status(404).json({
+			message: error.message,
+		});
+	}
 }
